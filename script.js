dc520c299f23ff0f81dcc4e835fe70bde209d436
molxinous <ayshani.choudhury@gmail.com>
2024-08-20 12:13:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-#
+document.addEventListener('DOMContentLoaded', () => {
+    const intro = document.querySelector('.intro');
+    setTimeout(() => {
+        intro.classList.add('active');
+    }, 500);
+});
